1f02be00b77eb0b031ea99c9aa0eb6f056ead113
Tib <shaik.tibout@outlook.com>
2023-05-26 18:40:37 +0400

    improve random

--- scripts/artwork.js
+++ scripts/artwork.js
@@ -1,6 +1,39 @@
 // Recommended class for randomness; remove unneeded functionality
 class Random {
   constructor() {
+    let offset = 0;
+    for (let i = 2; i < 66; i += 8) offset += parseInt(inputData.hash.substr(i, 8), 16);
+    offset %= 7;
+
+    const p = pos => parseInt(inputData.hash.substr((pos + offset), 8), 16);
+    let a = p(2) ^ p(34), b = p(10) ^ p(42), c = p(18) ^ p(50), d = p(26) ^ p(58) ^ p(2 + (8 - offset));
+
+    this.r = () => {
+      a |= 0; b |= 0; c |= 0; d |= 0;
+      let t = (((a + b) | 0) + d) | 0;
+      d = (d + 1) | 0; a = b ^ (b >>> 9);
+      b = (c + (c << 3)) | 0; c = (c << 21) | (c >>> 11);
+      c = (c + t) | 0;
+      return (t >>> 0) / 4294967296;
+    };
+    // 256 warmup cycles
+    for (let i = 0; i < 256; i++) this.r();
+  }
+  // Random decimal [0, 1)
+  random_dec = () => this.r();
+  // Random number [a, b)
+  random_num = (a, b) => a + (b - a) * this.random_dec();
+  // Random integer [a, b] (a < b required)
+  random_int = (a, b) => Math.floor(this.random_num(a, b + 1));
+  // Random boolean (p = true probability)
+  random_bool = (p) => this.random_dec() < p;
+  // Choose random item from array
+  random_choice = (list) => list[this.random_int(0, list.length - 1)];
+}
+
+
+/* class Random {
+  constructor() {
     let sfc32 = function (hex256) {
       let a = parseInt(hex256.substr(2, 8), 16) ^ parseInt(hex256.substr(34, 8), 16);
       let b = parseInt(hex256.substr(10, 8), 16) ^ parseInt(hex256.substr(42, 8), 16);
@@ -44,7 +77,7 @@ class Random {
     return list[this.random_int(0, list.length - 1)];
   }
 }
-
+ */
 function setup() {
   // Set aspect ratio
   let aspectRatio = 1.35;
